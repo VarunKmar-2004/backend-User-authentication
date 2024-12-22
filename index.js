58a8ac77956cf75910b73a1a6a831496f7a2c6ec
VarunKmar-2004 <143248587+VarunKmar-2004@users.noreply.github.com>
2024-12-22 18:09:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,16 @@ import userRouter from "./routes/UserRoutes.js"
 import cors from "cors"
 const app=express()
 app.use(express.json())
-const corsOptions = {
-  origin: ['https://frontend-user-authentication-fawn.vercel.app','http://localhost:3000'],
-  credentials: true,
-  allowedHeaders: ['Authorization', 'X-Custom-Header', 'Accept'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-};  
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions))
 app.use(cookieParser())
-app.use('/api/auth',authRouter);
-app.use('/api/user',userRouter);
+app.use(cors({
+    origin:['https://frontend-user-authentication-fawn.vercel.app','http://localhost:3000'],
+    credentials:true
+});
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
-
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 const port=process.env.PORT || 4000
 const Mongo_Uri='mongodb+srv://VarunKumar:iEj7K1z34xYINBxB@cluster1.cqnzl.mongodb.net/User_Auth'
 mongoose.connect(Mongo_Uri)

39e4bfc844da9087ed53d5bf7896538bf60c48c4
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2024-12-22 18:40:11 +0530

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 import express from "express"
 import mongoose from "mongoose"
+import cors from "cors"
 import authRouter from "./routes/authRoutes.js"
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/UserRoutes.js"
-import cors from "cors"
 const app=express()
 app.use(express.json())
-const corsOptions = {
+app.use(cors({
   origin: ['https://frontend-user-authentication-fawn.vercel.app','http://localhost:3000'],
-  credentials: true,
-  allowedHeaders: ['Authorization', 'X-Custom-Header', 'Accept'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-};  
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions))
+  credentials: true
+})); 
 app.use(cookieParser())
-app.use('/api/auth',authRouter);
-app.use('/api/user',userRouter);
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
-
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 const port=process.env.PORT || 4000
 const Mongo_Uri='mongodb+srv://VarunKumar:iEj7K1z34xYINBxB@cluster1.cqnzl.mongodb.net/User_Auth'
 mongoose.connect(Mongo_Uri)

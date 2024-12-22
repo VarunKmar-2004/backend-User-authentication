3189d4ad0e38e1f48cde1488b8a4958e937cb4df
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2024-12-22 14:40:16 +0530

    first commit

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ import userRouter from "./routes/UserRoutes.js"
 import cors from "cors"
 const app=express()
 app.use(express.json())
+app.use(cookieParser())
 const corsOptions = {
-  origin: 'https://frontend-user-authentication-fawn.vercel.app',
+  origin: ['https://frontend-user-authentication-fawn.vercel.app','http://localhost:3000'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 };  
 app.use(cors(corsOptions));
-app.use(cookieParser())
+app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://frontend-user-authentication-fawn.vercel.app','http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
-app.use('/auth',authRouter);
-app.use('/user',userRouter);
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 const port=process.env.PORT || 4000
 const Mongo_Uri='mongodb+srv://VarunKumar:iEj7K1z34xYINBxB@cluster1.cqnzl.mongodb.net/User_Auth'
 mongoose.connect(Mongo_Uri)

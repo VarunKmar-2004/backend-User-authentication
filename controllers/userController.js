a53c0a1fb5fb545a8a41a19781e06e69a45a87b6
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2024-12-23 09:51:18 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 import UserModal from "../models/model.js";
 export const getUser=async(req,res)=>{
-    const {userId}=req.body;
+    const {email}=req.body;
     try{
-    const user=await UserModal.findById(userId);
+    const user=await UserModal.findById(email);
     if(!user){
         return res.json({success:false,msg:'user not found'});
     }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,5 +2,5 @@ import express from "express";
 import UserId from "../middlewares/userId.js";
 import { getUser } from "../controllers/userController.js";
 const userRouter=express.Router();
-userRouter.get('/userdata',UserId,getUser);
+userRouter.get('/userdata',getUser);
 export default userRouter;
